9e631198140a57bca2908b9334ab10a9e8920dbd
Till Wehowski <github@webfan.de>
2024-09-07 02:55:01 +0200

    Create OIDplusPageWTFunctions.class.php

---
+++ OIDplusPageWTFunctions.class.php
@@ -0,0 +1,100 @@
+<?php
+
+/*
+ * OIDplus 2.0
+ * Copyright 2019 - 2023 Daniel Marschall, ViaThinkSoft
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Frdlweb\OIDplus\Plugins\AdminPages\WTFunctions;
+
+use ViaThinkSoft\OIDplus\Core\OIDplus;
+use ViaThinkSoft\OIDplus\Core\OIDplusException;
+use ViaThinkSoft\OIDplus\Core\OIDplusHtmlException;
+use ViaThinkSoft\OIDplus\Core\OIDplusPagePluginAdmin;
+
+// phpcs:disable PSR1.Files.SideEffects
+\defined('INSIDE_OIDPLUS') or die;
+// phpcs:enable PSR1.Files.SideEffects
+
+class OIDplusPageWTFunctions extends OIDplusPagePluginAdmin 
+{
+
+	/**
+	 * @param bool $html
+	 * @return void
+	 */
+	public function init(bool $html=true): void {
+		       
+		$isWPHooksFunctionsInstalled 
+		   = (//true === @\WPHooksFunctions::defined ||
+			  function_exists('add_action') ||
+			  \call_user_func_array(function(string $file){
+	  	            require_once $file;
+              return function_exists('add_action');	 
+           }, [	__DIR__.\DIRECTORY_SEPARATOR.'WPHooksFunctions.class.php',
+	     -1]));		
+
+		
+		  if(!$isWPHooksFunctionsInstalled){
+			 throw new \Exception('Could not init wp-functions-shim in '.__METHOD__.' '.__LINE__);  
+		  }
+		
+	    $io4Plugin = OIDplus::getPluginByOid("1.3.6.1.4.1.37476.9000.108.19361.24196");
+		if (!is_null($io4Plugin) ) {
+		    $Stubrunner = $io4Plugin->getWebfat(true,false);
+		}else{
+			$Stubrunner = null;
+		} 
+		//Pluginfiles
+		foreach(OIDplus::getAllPlugins() as $plugin){
+			$dir = $plugin->getPluginDirectory();
+			$file = rtrim($dir, '\\/ ').\DIRECTORY_SEPARATOR.'plugin.php';
+			if(file_exists($file)){
+				//this my be different from OIDplus Plugin manifest.json, mhh...???
+				$pData = \get_file_data($file, ['Name'=>'Plugin Name', 'Author'=>'Author', 'Version'=>'Version', 'License'=>'License',]);
+				if(count($pData) >= 3){
+					$fn = include $file;
+					if(is_callable($fn)){
+						if(!is_null($Stubrunner)){
+							$Stubrunner->call($fn);
+						}else{
+						    call_user_func($fn);	
+						}
+					}
+				}
+			}
+			
+		}	
+ 
+		    // userdata and tenant plugins, for OIDplus the are "anonympous"!?!
+			foreach( array_merge(glob(OIDplus::getUserDataDir('plugins')."*.php"),
+								 glob(OIDplus::getUserDataDir('plugins')."/*/plugin.php")
+					) as $file){
+				$pData = \get_file_data($file, ['Name'=>'Plugin Name', 'Author'=>'Author', 'Version'=>'Version', 'License'=>'License',]);
+				if(count($pData) >= 3){
+					$fn = include $file;
+					if(is_callable($fn)){			
+						if(!is_null($Stubrunner)){
+							$Stubrunner->call($fn);
+						}else{
+						    call_user_func($fn);	
+						}
+					}
+				}				
+			}		
+		
+	}
+	
+}
