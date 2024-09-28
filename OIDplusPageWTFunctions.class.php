06191f13c423bdad4d1ddc4490b01a8d79e2367f
Melanie Wehowski <github@webfan.de>
2024-09-28 21:48:37 +0200

    Update OIDplusPageWTFunctions.class.php
    + implements INTF_OID_1_3_6_1_4_1_37476_2_5_2_3_3
    + objectCMSPage()

--- OIDplusPageWTFunctions.class.php
+++ OIDplusPageWTFunctions.class.php
@@ -24,13 +24,230 @@ use ViaThinkSoft\OIDplus\Core\OIDplusException;
 use ViaThinkSoft\OIDplus\Core\OIDplusHtmlException;
 use ViaThinkSoft\OIDplus\Core\OIDplusPagePluginAdmin;
 use ViaThinkSoft\OIDplus\Core\OIDplusPagePluginPublic;
+use ViaThinkSoft\OIDplus\Core\OIDplusObject;
+use ViaThinkSoft\OIDplus\Plugins\PublicPages\Objects\INTF_OID_1_3_6_1_4_1_37476_2_5_2_3_3;
+use ViaThinkSoft\OIDplus\Plugins\PublicPages\Whois\INTF_OID_1_3_6_1_4_1_37476_2_5_2_3_4;
+use ViaThinkSoft\OIDplus\Plugins\PublicPages\Objects\INTF_OID_1_3_6_1_4_1_37476_2_5_2_3_2;
+use ViaThinkSoft\OIDplus\Plugins\AdminPages\Notifications\INTF_OID_1_3_6_1_4_1_37476_2_5_2_3_8;
+use ViaThinkSoft\OIDplus\Plugins\PublicPages\RestApi\INTF_OID_1_3_6_1_4_1_37476_2_5_2_3_9;
 
 // phpcs:disable PSR1.Files.SideEffects
 \defined('INSIDE_OIDPLUS') or die;
 // phpcs:enable PSR1.Files.SideEffects
 
 class OIDplusPageWTFunctions extends OIDplusPagePluginPublic 
+	implements INTF_OID_1_3_6_1_4_1_37476_2_5_2_3_3
+	
+	/* INTF_OID_1_3_6_1_4_1_37553_8_1_8_8_53354196964_1276945,
+	           INTF_OID_1_3_6_1_4_1_37476_2_5_2_3_2,//  modifyContent 
+	             INTF_OID_1_3_6_1_4_1_37476_2_5_2_3_9,  //API
+
+                   INTF_OID_1_3_6_1_4_1_37476_2_5_2_3_7,//public function getAlternativesForQuery(string $id): array;
+	 
+	           INTF_OID_1_3_6_1_4_1_37476_2_5_2_3_3, /* beforeObject*, afterObject* * /
+	           INTF_OID_1_3_6_1_4_1_37476_2_5_2_3_4, /* whois*Attributes * /
+	           INTF_OID_1_3_6_1_4_1_37476_2_5_2_3_8  /* getNotifications * /*/
 {
+	
+	
+	public const BODY_REPLACER = '@@@@BODYCONTENTREPLACER@@@@';			   	
+
+	
+				   
+  public function beforeObjectDelete(string $id): void {
+		$action = 'oidplus_'.__FUNCTION__;
+		  if(!did_action($action)){
+			  do_action($action, $id);
+		  }		
+  }
+
+	/**
+	 * Implements interface INTF_OID_1_3_6_1_4_1_37476_2_5_2_3_3
+	 * @param string $id
+	 * @return void
+	 * @throws OIDplusException
+	 */
+	public function afterObjectDelete(string $id): void {
+ 	 		
+		$action = 'oidplus_'.__FUNCTION__;
+		  if(!did_action($action)){
+			  do_action($action, $id);
+		  }		
+	}	
+	
+	
+	/**
+	 * Implements interface INTF_OID_1_3_6_1_4_1_37476_2_5_2_3_3
+	 * @param string $id
+	 * @param array $params
+	 * @return void
+	 */
+	public function beforeObjectUpdateSuperior(string $id, array &$params): void {
+	    global $oidplus_current_object_params;
+		
+		$oidplus_current_object_params = $params;
+		
+		$action = 'oidplus_'.__FUNCTION__;
+		  if(!did_action($action)){
+			  do_action($action, $id);
+		  }		
+		
+		$params = $oidplus_current_object_params;	
+	}
+
+	/**
+	 * Implements interface INTF_OID_1_3_6_1_4_1_37476_2_5_2_3_3
+	 * @param string $id
+	 * @param array $params
+	 * @return void
+	 */
+	public function afterObjectUpdateSuperior(string $id, array &$params): void {
+	    global $oidplus_current_object_params;
+		
+		$oidplus_current_object_params = $params;
+		
+		$action = 'oidplus_'.__FUNCTION__;
+		  if(!did_action($action)){
+			  do_action($action, $id);
+		  }		
+		
+		$params = $oidplus_current_object_params;	
+	}
+
+	/**
+	 * Implements interface INTF_OID_1_3_6_1_4_1_37476_2_5_2_3_3
+	 * @param string $id
+	 * @param array $params
+	 * @return void
+	 */
+	public function beforeObjectUpdateSelf(string $id, array &$params): void {
+	    global $oidplus_current_object_params;
+		
+		$oidplus_current_object_params = $params;
+		
+		$action = 'oidplus_'.__FUNCTION__;
+		  if(!did_action($action)){
+			  do_action($action, $id);
+		  }		
+		
+		$params = $oidplus_current_object_params;	
+	}
+
+	/**
+	 * Implements interface INTF_OID_1_3_6_1_4_1_37476_2_5_2_3_3
+	 * @param string $id
+	 * @param array $params
+	 * @return void
+	 */
+	public function afterObjectUpdateSelf(string $id, array &$params): void {
+	    global $oidplus_current_object_params;
+		
+		$oidplus_current_object_params = $params;
+		
+		$action = 'oidplus_'.__FUNCTION__;
+		  if(!did_action($action)){
+			  do_action($action, $id);
+		  }		
+		
+		$params = $oidplus_current_object_params;	
+	}
+
+	/**
+	 * Implements interface INTF_OID_1_3_6_1_4_1_37476_2_5_2_3_3
+	 * @param string $id
+	 * @param array $params
+	 * @return void
+	 */
+	public function beforeObjectInsert(string $id, array &$params): void {
+	    global $oidplus_current_object_params;
+		
+		$oidplus_current_object_params = $params;
+		
+		$action = 'oidplus_'.__FUNCTION__;
+		  if(!did_action($action)){
+			  do_action($action, $id);
+		  }		
+		
+		$params = $oidplus_current_object_params;	
+	}
+
+	/**
+	 * Implements interface INTF_OID_1_3_6_1_4_1_37476_2_5_2_3_3
+	 * @param string $id
+	 * @param array $params
+	 * @return void
+	 */
+	public function afterObjectInsert(string $id, array &$params): void {
+	    global $oidplus_current_object_params;
+		
+		$oidplus_current_object_params = $params;
+		
+		$action = 'oidplus_'.__FUNCTION__;
+		  if(!did_action($action)){
+			  do_action($action, $id);
+		  }		
+		
+		$params = $oidplus_current_object_params;
+	}
+			   
+	
+	
+	
+    public static function objectCMSPage(string | OIDplusObject $obj, ?bool $verbose = false, ?bool $die = false, ?string $id = null){
+		global $oidplus_current_object_id;
+		global $oidplus_current_page_context;
+		global $oidplus_current_page_verbose;
+		//print_r($obj);die();
+		$page  = \frdl_ini_dot_parse(is_string($obj) ? $obj : $obj->getDescription(), true);
+		$page['data']['id'] = is_string($obj) ? $id : $obj->nodeId();
+		//$data = $page['data']; 
+		// print_r($data);die();
+	 
+		
+		$oidplus_current_object_id = $page['data']['id'];
+		$oidplus_current_page_context = $page;
+		$oidplus_current_page_verbose= $verbose;
+			
+		$html = $page['content']; 
+		
+		  if(!did_action('oidplus_prepare_shortcode')){
+			  do_action('oidplus_prepare_shortcode', __METHOD__);
+		  }
+		$html = \do_shortcode($html);		
+		$page['html'] = $html;
+		
+		unset($oidplus_current_object_id);
+		unset($oidplus_current_page_context);
+		unset($oidplus_current_page_verbose);
+		
+		if(true === $verbose){
+			$format = isset($_GET['format']) ? $_GET['format'] : 'cms';
+			 
+			switch($format){
+				case 'json' :
+					 header('Content-Type: application/json');
+					 echo json_encode($page, \JSON_PRETTY_PRINT);
+					break;
+				case 'cms' ===$format && function_exists('\io4\container') :
+					 $HtmlCompiler = \io4\container()->get('HtmlDocument') ;
+					 $tpl = $HtmlCompiler->compile(static::BODY_REPLACER); 
+					 $pageHTML = str_replace(static::BODY_REPLACER, $page['html'], $tpl);
+					 echo $pageHTML;
+					break;
+				case 'html' :
+				case 'body' :
+					default :
+					  echo $page['html'] ;
+					break;
+			}			
+		}
+		if(true === $die){
+			die();
+		}
+		return $page;
+	}		
+	
+	
    public function gui(string $id, array &$out, bool &$handled): void {
 	   global $oidplus_public_pages_gui_id;
        global $oidplus_public_pages_gui_out;
