cf7944fc398f2974431e8ca433ff4a260c76974c
Melanie Wehowski <github@webfan.de>
2024-10-24 13:43:56 +0200

    Update oidplus-functions.php

--- oidplus-functions.php
+++ oidplus-functions.php
@@ -2,6 +2,23 @@
 
 namespace {
 	
+ function oidplus_format_bytes($bytes, $precision = 2)
+	{
+		$units = array('B', 'KB', 'MB', 'GB', 'TB');
+
+		$bytes = max($bytes, 0);
+		$pow = floor(($bytes ? log($bytes) : 0) / log(1024));
+		$pow = min($pow, count($units) - 1);
+
+		// Uncomment one of the following alternatives
+		  $bytes /= pow(1024, $pow);
+		// $bytes /= (1 << (10 * $pow));
+
+		return round($bytes, $precision) . $units[$pow];
+	}	
+	
+	
+	
  function oidplus_quota_used_db(){
 		$sum = 0;
 		$q="SELECT table_name AS `table`,
