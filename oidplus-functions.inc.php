28df787b33255dc84d097b066dd8d6d63f544104
Melanie Wehowski <github@webfan.de>
2024-11-12 19:00:48 +0100

    Update oidplus-functions.inc.php
    prepare for other SQLlangs

--- oidplus-functions.inc.php
+++ oidplus-functions.inc.php
@@ -1,7 +1,12 @@
 <?php
 
 namespace {
+	
+	use ViaThinkSoft\OIDplus\Core\OIDplus;
+	use ViaThinkSoft\OIDplus\Core\OIDplusException;
+	
 \defined('INSIDE_OIDPLUS') or die;	
+	
  function oidplus_format_bytes(int | float $bytes, int $precision = 2)
 	{
 		$units = array('B', 'KB', 'MB', 'GB', 'TB');
@@ -21,16 +26,38 @@ namespace {
 	
  function oidplus_quota_used_db(){
 		$sum = 0;
-		$q="SELECT table_name AS `table`,
+	 if (OIDplus::db()->getSlang()->id() == 'mysql') {
+			$q="SELECT table_name AS `table`,
 ROUND(((data_length + index_length) / 1024 / 1024), 2) AS `megabyte`
 FROM information_schema.TABLES
-WHERE table_schema = ? AND table_name LIKE '".str_replace('_', '\_', \ViaThinkSoft\OIDplus\Core\OIDplus::baseConfig()->getValue('TABLENAME_PREFIX'))."%'
+WHERE table_schema = ? AND table_name LIKE '".str_replace('_', '\_', OIDplus::baseConfig()->getValue('TABLENAME_PREFIX'))."%'
 ORDER BY (data_length + index_length) DESC";
-		
-		//  die($q);
+			} else if (OIDplus::db()->getSlang()->id() == 'mssql') {
+				$q=false;
+			} else if (OIDplus::db()->getSlang()->id() == 'oracle') {
+				$q=false;
+			} else if (OIDplus::db()->getSlang()->id() == 'pgsql') {
+				$q=false;
+			} else if (OIDplus::db()->getSlang()->id() == 'access') {
+				$q=false;
+			} else if (OIDplus::db()->getSlang()->id() == 'sqlite') {
+				$q=false;
+			} else if (OIDplus::db()->getSlang()->id() == 'firebird') {
+				$q=false;
+			} else {
+				$q=false;
+			}
+		 
+	   if(false === $q){
+		   throw new OIDplusException(sprintf('SQLlang %s is not supported yet in function %s!', 
+											  OIDplus::db()->getSlang()->id(), 
+											  __FUNCTION__), 
+									          'Unsupported SQL language for this service',
+									          409);
+	   }
 	
-		$resQ = \ViaThinkSoft\OIDplus\Core\OIDplus::db()->query($q, [	
-			\ViaThinkSoft\OIDplus\Core\OIDplus::baseConfig()->getValue('MYSQL_DATABASE'), 
+		$resQ = OIDplus::db()->query($q, [	
+			OIDplus::baseConfig()->getValue('MYSQL_DATABASE'), 
 	  ]);
 		$t = [];
 		while ($row = $resQ->fetch_array()) { 
